0aeba890a0635418aed4f3c8e91bc48a163e71bb
Clay <38699310+clayclay360@users.noreply.github.com>
2021-11-15 17:20:30 -0500

    Commented code

--- WheelHero/Assets/scripts/bullet_controller.cs
+++ WheelHero/Assets/scripts/bullet_controller.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class bullet_controller : MonoBehaviour
 {
+    //variables
     [Header("Bullet Info")]
     public SpriteRenderer sprite_renderer;
     public float time;
@@ -11,11 +12,14 @@ public class bullet_controller : MonoBehaviour
     private bool is_hit;
     private Collider col;
 
+    //get the collider
     private void Start()
     {
         col = GetComponent<Collider>();
     }
 
+    /*if the bullet is hit then interpolate the color and 
+    alpha to a clear white, then destroy the bullet when it's barely visible*/
     private void Update()
     {
         if (is_hit)
@@ -29,6 +33,9 @@ public class bullet_controller : MonoBehaviour
         }
     }
 
+    /*if the bullet hits another collider, is hit equals true
+    if the colliders tag is "Wheel" or "Ground" tag the bullet "Dead Bullet"
+    else if the tag is "Player" and the bullet's tag is still "Bullet" then kill the player*/
     private void OnCollisionEnter2D(Collision2D collision)
     {
         is_hit = true;

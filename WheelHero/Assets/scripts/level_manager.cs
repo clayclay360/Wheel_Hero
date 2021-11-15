1ee1d82291ef4f69690157af463448ad971fb2ef
Clay <38699310+clayclay360@users.noreply.github.com>
2021-11-15 17:24:18 -0500

    Commented code

--- WheelHero/Assets/scripts/level_manager.cs
+++ WheelHero/Assets/scripts/level_manager.cs
@@ -1,19 +1,22 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class level_manager : MonoBehaviour
 {
+    //variables 
     public int number_of_enemies;
     public int next_level;
     SpriteRenderer sprite_renderer;
 
+    //get sprite renderer
     private void Start()
     {
         sprite_renderer = GetComponent<SpriteRenderer>();
     }
-
+    
+    //if the # of enemies equals 0 then turn the panel green
     void Update()
     {
         if(number_of_enemies == 0)
@@ -21,7 +24,9 @@ public class level_manager : MonoBehaviour
             sprite_renderer.color = Color.green;
         }
     }
-
+    
+    /*if the # of enemies equals 0 and the player is staying on
+    the panel, then load the next level*/
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (number_of_enemies == 0 && collision.gameObject.tag == "Player")
@@ -29,7 +34,8 @@ public class level_manager : MonoBehaviour
             load_level(next_level);
         }
     }
-
+    
+    //function for loading the next lexel
     private void load_level(int level)
     {
         SceneManager.LoadScene(level, LoadSceneMode.Single);

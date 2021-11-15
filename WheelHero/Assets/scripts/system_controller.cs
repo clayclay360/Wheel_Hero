8a318f983876cf364a6bf2691e79edea5350989a
Clay <38699310+clayclay360@users.noreply.github.com>
2021-11-15 18:04:43 -0500

    Commented code

--- WheelHero/Assets/scripts/system_controller.cs
+++ WheelHero/Assets/scripts/system_controller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class system_controller : MonoBehaviour
 {
+    //variables
     public GameObject door;
     public float y_target_pos;
     public float time;
@@ -12,11 +13,14 @@ public class system_controller : MonoBehaviour
     private SpriteRenderer sprite_renderer;
     private float reference;
     private float current_y;
+    
     void Start()
     {
         current_y = door.transform.position.y;
         sprite_renderer = GetComponent<SpriteRenderer>();
     }
+    
+    //smoothly change the y position of the door depending on the boolen door open
     void Update()
     {
         if (door_open)
@@ -33,6 +37,7 @@ public class system_controller : MonoBehaviour
         }
     }
 
+    //if hit by the wheel then change the condition of door open to the opposite of what it is
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Wheel")
